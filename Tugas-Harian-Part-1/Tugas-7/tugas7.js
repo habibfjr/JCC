951f330c9f34c67fd49665c09c90c8addc03604a
habibfjr <habibf99@gmail.com>
2022-03-15 20:59:01 +0700

    update tugas 7

--- Tugas-Harian-Part-1/Tugas-7/tugas7.js
+++ Tugas-Harian-Part-1/Tugas-7/tugas7.js
@@ -22,8 +22,10 @@ console.log('\n')
 // Soal 3
 {
     let array2 = [1, 2, 3, 4, 5, 6,7, 8, 9, 10]
-    for(let i=1; i<array2.length; i+=2){
-        console.log(array2[i])
+    for(let i=0; i<array2.length; i++){
+        if(array2[i]%2 === 0){
+            console.log(array2[i])
+        }
     }
 }
 console.log('\n')
