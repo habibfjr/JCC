6758ee14525678155d582a3f0ac0a9c12192abc2
habibfjr <habibf99@gmail.com>
2022-03-19 13:52:27 +0700

    add tugas 10

---
+++ tugas-harian-part-2/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import logo from './assets/img/logo.png';
+import './App1.css';
+
+const App=()=>{
+
+  const ToDoList=(props)=>{
+    return(
+      <div className='toDoList'>
+        <input type={'checkbox'}/>{props.text}
+        <hr/>
+      </div>   
+    )
+  }
+
+  return(
+    <div className='card'>
+      <img src={logo} />
+      <h1>THINGS TO DO</h1>
+      <p>During Bootcamp in Jabarcodingcamp</p>
+      <hr/>
+
+      <div className='containerList'></div>
+        <ToDoList text={'Belajar GIT & CLI'}/>
+        <ToDoList text={'Belajar HTML & CSS'}/>
+        <ToDoList text={'Belajar Javascript'}/>
+        <ToDoList text={'Belajar ReactJS Dasar'}/>
+        <ToDoList text={'Belajar ReactJS Advance'}/>
+    
+      <div className='button'>
+        <input type={'submit'} name={'send'} value={'Send'} />
+      </div>
+    </div>
+  )
+}
+
+export default App;
